5440b5ee69bef913659d7c823372a7447ed43c4b
rdcarrera <rdcdc88@gmail.com>
2018-10-30 01:13:03 +0100

    - Solved problems to safe the yaml history data - Added an interval time  between checks

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-import sys, yaml, importlib, getopt, time, threading
-import os.path
-import random
+import sys, yaml, importlib, getopt, datetime, threading, os
+from dateutil import parser 
 
 CONFIG_DIRECTORY = "./config/"
-HISTORY_DIRECOTRY = "./history/"
+HISTORY_DIRECTORY = "./history/"
+SECONDS_INTERVAL = 300
 
 class StepData (object):
     def run(self):
@@ -15,15 +15,39 @@ class StepData (object):
             return("[CRITICAL] - The module file doesn't exist, please check ./modules/"+self.module_name+".py",2)
         module = importlib.import_module('modules.'+self.module_name)
         return module.main(config_file)
-    def __init__(self,module_name, config_name):
+    def returninfo(self):
+        return { 
+            'config_name': self.config_name, 
+            'module_name': self.module_name, 
+            'execution_time': self.execution_time, 
+            'result_info': 
+            [ 
+                self.result_info[0],
+                self.result_info[1] 
+            ] 
+            }
+    def __init__(self,module_name, config_name, result_file):
         self.module_name = module_name
         self.config_name = config_name
         self.result_info = self.run()
-        self.execution_time = time.time()
-        result_file = HISTORY_DIRECOTRY+config_name+".yml"
+        self.execution_time = datetime.datetime.now()
         with open(result_file, 'w') as _outfile:
-            yaml.dump(self, _outfile, default_flow_style=False)
-        print self.result_info[0]
+            yaml.dump(self.returninfo(), _outfile, default_flow_style=False)
+        
+def time_comparation(result_file,external_config):
+    if os.path.isfile(result_file):
+        if "seconds_interval" not in external_config:
+            seconds_interval = SECONDS_INTERVAL
+        else:
+            seconds_interval = external_config["seconds_interval"]
+    result_file_load = open(result_file,'r')
+    result_file_data = yaml.safe_load(result_file_load)
+    yaml.load_all
+    next_execution_time = result_file_data["execution_time"]+datetime.timedelta(seconds=seconds_interval)
+    if datetime.datetime.now() < next_execution_time:
+        return False
+    return True
+
 
 def main(argv):
     health_check_config = ''
@@ -40,13 +64,21 @@ def main(argv):
     if os.path.isfile(health_check_config) is False:
         print "[CRITICAL] - The config of the health check proccess doesn't exist, please check "+health_check_config
         sys.exit(2)
+    if not os.path.exists(HISTORY_DIRECTORY):
+        os.makedirs(HISTORY_DIRECTORY)
+
     config_file_load = open(health_check_config,'r')
     config = yaml.safe_load(config_file_load)
 
     try:
         for _iterator in range(len(config["tasks"])):
             for _iterator2 in range(len(config["tasks"][_iterator]["steps"])):
-                threading.Thread(target=StepData, args=(config["tasks"][_iterator]["steps"][_iterator2]["module"],config["tasks"][_iterator]["steps"][_iterator2]["name"])).start()
+                result_file = HISTORY_DIRECTORY+config["tasks"][_iterator]["steps"][_iterator2]["name"]+".yml"
+                if time_comparation(result_file,config["tasks"][_iterator]["steps"][_iterator2]) is False:
+                    continue
+
+                threading.Thread(target=StepData, args=(config["tasks"][_iterator]["steps"][_iterator2]["module"],config["tasks"][_iterator]["steps"][_iterator2]["name"],result_file)).start()
+
     except (KeyError,TypeError) as _error:
         print "[CRITICAL] - The config file "+health_check_config+" is invalid"
         sys.exit(2)
